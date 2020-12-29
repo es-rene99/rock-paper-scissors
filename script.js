c1da243704c0611c65b36712317ee6f07844f346
es-rene99 <es.rene99@gmail.com>
2020-12-29 07:34:49 -0600

    refactor: applied ESlint
    refactor: updating project, removed 5 rounds logic

--- script.js
+++ script.js
@@ -1,69 +1,45 @@
-let responseType = [
-    "rock",
-    "paper",
-    "scissors"
-]
+const responseType = [
+  'rock',
+  'paper',
+  'scissors',
+];
 let errorFlag = false;
 
-function computerPlay(){
-    let randomResponse = responseType[Math.floor((responseType.length * Math.random()))];
-    console.log(`The computer picks ${randomResponse}`);
-    return randomResponse;
+function computerPlay() {
+  const randomResponse = responseType[Math.floor((responseType.length * Math.random()))];
+  console.log(`The computer picks ${randomResponse}`);
+  return randomResponse;
 }
 
-function userPlay(){
-    let userResponse = prompt('Rock, paper, scissors! (you picked:)');
-    console.log(`The player picks ${userResponse}`);
-    if(responseType.includes(userResponse)){
-        errorFlag = false;
-        return userResponse;
-    } else {
-        errorFlag = true;
-        return "";
-    }
+function userPlay() {
+  const userResponse = prompt('Rock, paper, scissors! (you picked:)');
+  console.log(`The player picks ${userResponse}`);
+  if (responseType.includes(userResponse)) {
+    errorFlag = false;
+    return userResponse;
+  }
+  errorFlag = true;
+  return '';
 }
 
-function singleRound(playerSelection, computerSelection){
-    if(errorFlag == true) return "Not a valid answer";
-    if (playerSelection == "scissors" && computerSelection == "paper" ||
-        playerSelection == "rock" && computerSelection == "scissors" ||
-        playerSelection == "paper" && computerSelection == "rock"
-    ){
-        return "The player wins"
-    }
-    else if(playerSelection == computerSelection){
-        return "Tie";
-    }
-    else{
-        return "The computer wins"
-    }
-}
-function game(){
-    let userScore = 0;
-    let computerScore = 0;
-    let winRoundMessage;
-    for(round = 1; round <= 5; round++){
-        winRoundMessage = singleRound(userPlay(),computerPlay());
-        switch(winRoundMessage){
-            case "The player wins": 
-                userScore++;
-                break;
-            case "The computer wins": 
-                computerScore++;
-                break;
-            case "Not a valid answer":
-            case "Tie": 
-                console.log("Try again");
-                round--;
-                break;
-            default: 
-            console.log("An error has ocurred...");        
-        }
-        console.log(winRoundMessage);
-        console.log(`User Score: ${userScore}`);
-        console.log(`Computer Score: ${computerScore}`);
-    }
-    let definitiveWinnerMessage = (userScore>computerScore)?"The playe wins the game!":"The computer wins the game!";
-    console.log(definitiveWinnerMessage);
-}
+function singleRound(playerSelection, computerSelection) {
+  if (errorFlag === true) return 'Not a valid answer';
+  if ((playerSelection === 'scissors' && computerSelection === 'paper')
+    || (playerSelection === 'rock' && computerSelection === 'scissors')
+    || (playerSelection === 'paper' && computerSelection === 'rock')
+  ) {
+    return 'The player wins';
+  }
+  if (playerSelection === computerSelection) {
+    return 'Tie';
+  }
 
+  return 'The computer wins';
+}
+function game() {
+  const userScore = 0;
+  const computerScore = 0;
+  let winRoundMessage;
+  const definitiveWinnerMessage = (userScore > computerScore) ? 'The playe wins the game!' : 'The computer wins the game!';
+  console.log(definitiveWinnerMessage);
+}
